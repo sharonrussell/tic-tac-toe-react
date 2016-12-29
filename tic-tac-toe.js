3eb9f7cc99715d7e9c0e6795447830bbbfc3a243
sharonrussell <sharon.russell@comparethemarket.com>
2016-12-29 12:17:37 +0000

    refactor to make the board responsible for the state of the squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,22 +1,27 @@
-class Square extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            value: null
-        };
-    }
-    render() {
-        return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
-            </button>
-        );
-    }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
     render() {
         const status = 'Next player: X';
